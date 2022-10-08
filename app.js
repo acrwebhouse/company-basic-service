69e0346d9c6bb45fab2df16619b8aa622430e14c
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-10-08 22:13:06 +0800

    add company

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const mongoDBPort = config.mongoDBPort;
 const mongoDBName = config.mongoDBName;
 const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
-const userRestApi = require("./lib/rest_api/user");
+const companyRestApi = require("./lib/rest_api/company");
 console.log(config)
 dataBaseInit.mongoDBInit(mongoDBPort,mongoDBName);
 serverUse.on(app);
-userRestApi.on(app);
+companyRestApi.on(app);
 server.listen(process.env.PORT||serverPort);
 console.log("現在使用" + serverPort + "port");
